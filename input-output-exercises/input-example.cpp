ea68ee8df23272ec639f54d942c8fb70f486b13f
maggie <myustinreyes@gmail.com>
2015-04-14 20:28:39 -0600

    complete basic example

--- input-output-exercises/input-example.cpp
+++ input-output-exercises/input-example.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 /*
 Other optional methods to use:
 using namespace std;
@@ -11,10 +10,14 @@ using std::cin;
 using std::endl;
 */
 int main(){
-	string name = "";
+	//Here I'm using a string variable called name.
+	//Strings are especially interesting in C++ as we'll learn later.
+	std::string name;
 
-	std::cout << "What is your name?" << std::endl
+	std::cout << "What is your name?" << std::endl;
+	//This line prompts for a user to input information. Here we'll be storing that info in our new variable
 	std::cin >> name;
 
-	std::cout << "Hello, " << name << "and welcome to the world!" << std::endl;
+	std::cout << "Hello, " << name << " and welcome to the world!" << std::endl;
+	//That's all there is to input!
 }
