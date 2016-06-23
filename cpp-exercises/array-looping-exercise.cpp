63fc085ac8f7b2d8eb061d2eb51d02e99baf67a7
maggie <myustinreyes@gmail.com>
2016-06-23 11:04:14 -0400

    type out explanations for exercises

--- cpp-exercises/array-looping-exercise.cpp
+++ cpp-exercises/array-looping-exercise.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std; 
 
+/* in this exercise, I want you to create an array at least 5 elements long
+and using a loop cout each value in the array. the array can be of any
+variable type. 
+
+	what loop could you use to accomplish this task?
+*/
+
 int main(){
 	cout << "Hello World!";
 }
--- cpp-exercises/output-diamond-exercise.cpp
+++ cpp-exercises/output-diamond-exercise.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std; 
 
+/* in this exercise I want you to print a diamond to the screen out of *'s
+based on how large the user wants the diamond to be. 
+
+	will you have to loop to create the diamond? how will the loop work and
+	what type should you use?
+*/
+
 int main(){
 	cout << "Hello World!";
 }
